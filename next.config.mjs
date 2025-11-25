476b05fddff4f14d4d2f82a05f5e35f9c86eddbd
mwihoti <danielmwihoti@gmail.com>
2025-11-25 22:49:53 +0300

    Fix Vercel build errors - exclude hardhat config from TypeScript checking and ignore optional dependencies

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config, { isServer }) => {
+    config.ignoreWarnings = [
+      ...(config.ignoreWarnings || []),
+      // Ignore missing optional peer dependencies
+      {
+        module: /node_modules\/@metamask\/sdk/,
+      },
+    ];
+    return config;
+  },
+};
 
 export default nextConfig;
